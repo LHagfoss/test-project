e44ff530ae974ebde189a09e8645f143ed8a67e9
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-11-13 14:36:36 +0100

    animation for all text on intro Component

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FadeIn from "./fadeIn";
+import AnimatedText from "./animatedText";
 
 export default function Intro() {
     return (
@@ -8,13 +9,13 @@ export default function Intro() {
                 <div className="container m-5 md:mx-20 flex justify-between items-center text-[--text]">
                     <div className="w-full flex flex-col justify-between h-[60vh] text-[--Text]">
                         <div className="flex flex-col leading-tight">
-                            <h1 className="text-[9vw] md:text-[4vw] font-bold   ">IT-Avdeling Kommunen</h1>
-                            <h3 className="text-[8vw] md:text-[2.5vw] font-medium">Rådhusgata 26</h3>
-                            <h4 className="text-[3.5vw] md:text-[1vw]">11. November -{'>'} 22. November</h4>
+                            <h1 className="text-[9vw] md:text-[4vw] font-bold"><AnimatedText text="IT-Avdeling Kommunen" delay={0.1} /></h1>
+                            <h3 className="text-[8vw] md:text-[2.5vw] font-medium"><AnimatedText text="Rådhusgata 26" delay={0.2} /></h3>
+                            <h4 className="text-[3.5vw] md:text-[1vw]"><AnimatedText text="11. November -{'>'} 22. November" delay={0.3} /></h4>
                         </div>
                         <div className="w-full flex justify-between text-[6vw] md:text-[1vw]">
-                            <h1 className="">Laget av <span className="text-[--primary] underline cursor-pointer"> Lucas Hagfoss</span></h1>
-                            <h1 className="">Scroll down</h1>
+                            <h1 className=""><AnimatedText text="Laget av " delay={0.4} /> <span className="text-[--primary] underline cursor-pointer"> <AnimatedText text="Lucas Hagfoss" delay={0.9} /></span></h1>
+                            <h1 className=""><AnimatedText text="Scroll down" delay={1.5} /></h1>
                         </div>
                     </div>
                 </div>
--- src/components/scrollProgress.tsx
+++ src/components/scrollProgress.tsx
@@ -2,12 +2,12 @@ import { motion, useViewportScroll, useTransform } from "framer-motion"
 
 export default function ScrollProgress() {
     const { scrollYProgress } = useViewportScroll()
-    const height = useTransform(scrollYProgress, [0, 1], ['0vh', '100vh'])
+    const height = useTransform(scrollYProgress, [0, 1], ['0.8vh', '98.5vh'])
 
     return (
         <motion.div
             style={{ height }}
-            className="fixed top-1 left-1 w-2 bg-[--accent] z-10 rounded-full"
+            className="fixed top-2 left-2 w-2 bg-[--accent] z-10 rounded-full"
         />
     )
 }
